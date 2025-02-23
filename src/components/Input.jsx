8ff8ab10c53ce92b850113fdde84af92728638c8
Imam Taufiq Hermawan (2308440027) <2308440027@prodiacorp.com>
2025-02-23 15:07:17 +0700

    styling input and buttons

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,17 @@
-function Input({ textArea, label }) {
+function Input({ textArea, label, ...props }) {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
   return (
-    <p>
-      <label htmlFor="">{label}</label>
-      {textArea ? <textarea name="" id=""></textarea> : <input type="text" />}
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500" htmlFor="">
+        {label}
+      </label>
+      {textArea ? (
+        <textarea className={classes} {...props}></textarea>
+      ) : (
+        <input className={classes} {...props} />
+      )}
     </p>
   );
 }
