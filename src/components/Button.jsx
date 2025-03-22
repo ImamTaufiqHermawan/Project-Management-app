d07b164f1c46905fc7322494aba8779589e2a394
ImamTaufiqHermawan <imamtaufiq133@gmail.com>
2025-03-22 12:59:53 +0700

    manage state between components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
 function Button({ children, ...props }) {
   return (
-    <button className="px-4 py-2 text-xs md:text-base rounded-mg bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+    <button
+      className="px-4 py-2 text-xs md:text-base rounded-mg bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
+      {...props}
+    >
       {children}
     </button>
   );
